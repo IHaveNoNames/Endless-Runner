e33de0c69a6b80e9994bd79f1ef37c2e669b4e3b
IHaveNoNames <leungweijun@gmail.com>
2018-12-14 02:06:00 +0800

    Upload Project

---
+++ Endless Runner/Assets/Player/Platform.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour {
+
+    //GameObject[] leftPlatforms;
+    //GameObject[] platforms;
+    //GameObject[] rightPlatforms;
+
+    [SerializeField]
+    Transform leftPlatformPrefab;
+    [SerializeField]
+    Transform platformPrefab;
+    [SerializeField]
+    Transform rightPlatformPrefab;
+
+    Queue<Transform> platformQueue;
+    Queue<Transform> platformLeftQueue;
+    Queue<Transform> platformRightQueue;
+
+    Vector3 startLeftPos = new Vector3(-5, 0, 0);
+    Vector3 startPos = new Vector3(0, 0, 0);
+    Vector3 startRightPos = new Vector3(5, 0, 0);
+
+    Vector3 nextLeftPos;
+    Vector3 nextPos;
+    Vector3 nextRightPos;
+
+    int noOfPlatforms = 5;  
+
+	// Use this for initialization
+	void Start () {
+        //leftPlatforms = GameObject.FindGameObjectsWithTag("Left Platform");
+        //platforms = GameObject.FindGameObjectsWithTag("Platform");
+        //rightPlatforms = GameObject.FindGameObjectsWithTag("Right Platform");
+
+        platformLeftQueue = new Queue<Transform>(noOfPlatforms);
+        platformQueue = new Queue<Transform>(noOfPlatforms);
+        platformRightQueue = new Queue<Transform>(noOfPlatforms);
+
+
+        for(int i = 0; i<noOfPlatforms; i++)
+        {
+            platformLeftQueue.Enqueue((Transform)Instantiate(leftPlatformPrefab));
+            platformQueue.Enqueue((Transform)Instantiate(platformPrefab));
+            platformRightQueue.Enqueue((Transform)Instantiate(rightPlatformPrefab));
+        }
+
+        nextLeftPos = startLeftPos;
+        nextPos = startPos;
+        nextRightPos = startRightPos;
+
+
+        for (int i = 0; i<noOfPlatforms; i++)
+        {
+            Recycle(ref nextLeftPos, ref platformLeftQueue);
+            Recycle(ref nextPos, ref platformQueue);
+            Recycle(ref nextRightPos, ref platformRightQueue);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (platformQueue.Peek().localPosition.z + 25f < Camera.main.transform.position.z)
+        {
+            Recycle(ref nextLeftPos, ref platformLeftQueue);
+            Recycle(ref nextPos, ref platformQueue);
+            Recycle(ref nextRightPos, ref platformRightQueue);
+        }
+	}
+
+    void Recycle(ref Vector3 nextPosition, ref Queue<Transform> queue)
+    {
+        //Vector3 leftPos = nextLeftPos;
+        //Vector3 position = nextPos;
+        //Vector3 rightPos = nextRightPos;
+
+        Vector3 position = nextPosition;
+
+        //leftPos.z += 25f * 0.5f;
+        //position.z += 25f * 0.5f;
+        //rightPos.z += 25f * 0.5f;
+
+        position.z += 25f * 0.5f;
+
+        //Transform leftPlatform = platformLeftQueue.Dequeue();
+        //Transform platform = platformQueue.Dequeue();
+        //Transform rightPlatform = platformRightQueue.Dequeue();
+
+        Transform platform = queue.Dequeue();
+
+        platform.position = position;
+
+        //leftPlatform.position = leftPos;
+        //platform.position = position;
+        //rightPlatform.position = rightPos;
+
+        nextPosition.z += 25f;
+
+        //nextLeftPos.z += 25f;
+        //nextPos.z += 25f;
+        //nextRightPos.z += 25f;
+
+        Color newColor = new Color(Random.value, Random.value, Random.value, 1.0f);
+        platform.gameObject.GetComponent<Renderer>().material.color = newColor;
+
+
+        queue.Enqueue(platform);
+
+        //platformLeftQueue.Enqueue(leftPlatform);
+        //platformQueue.Enqueue(platform);
+        //platformRightQueue.Enqueue(rightPlatform);
+        
+    }
+}
---
+++ Endless Runner/Assets/Player/Player.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float acceleration = 20f;
+
+    Rigidbody rb;
+
+    Vector3 endPos;
+
+    Vector3 startPos;
+
+    float durationOfLerp = 0.2f;
+
+    float distanceToMove = 5f;
+
+    bool isLerping = false;
+
+    float timeStarted;
+
+    Vector3 jumpVelocity = new Vector3(0, 5f, 0f);
+
+    private Animator anim;
+
+    //Ground Checking
+    [SerializeField]
+    Transform groundCheck;
+    float groundRadius = 0.1f;
+    [SerializeField]
+    LayerMask whatIsGround;
+    [SerializeField]
+    bool onGround;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (!isLerping && transform.position.x < 5)
+            {
+                StartLerping(Vector3.right);
+            }
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (!isLerping && transform.position.x > -5)
+            {
+                StartLerping(Vector3.left);
+            }
+        }
+
+        if (Input.GetButtonDown("Jump") && onGround)
+        {
+            anim.SetTrigger("Jump");
+            rb.AddForce(jumpVelocity, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            anim.SetTrigger("Slide");
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(0f, 0f, acceleration, ForceMode.Acceleration);
+        onGround = isGrounded();
+        anim.SetBool("Run", true);
+
+        if (isLerping)
+        {
+            float timeSinceStarted = Time.time - timeStarted;
+            float percentageComplete = timeSinceStarted / durationOfLerp;
+
+            Vector3 newPos = Vector3.Lerp(startPos, endPos, percentageComplete);
+            transform.position = new Vector3(newPos.x, transform.position.y, transform.position.z);
+
+            if (percentageComplete >= 1.0f)
+            {
+                isLerping = false;
+            }
+        }
+
+        
+    }
+
+    void StartLerping(Vector3 direction)
+    {
+        isLerping = true;
+        timeStarted = Time.time;
+
+        startPos = transform.position;
+        endPos = transform.position + direction * distanceToMove;
+
+    }
+
+    bool isGrounded()
+    {
+        return Physics.CheckSphere(groundCheck.position, groundRadius, whatIsGround);
+    }
+}
