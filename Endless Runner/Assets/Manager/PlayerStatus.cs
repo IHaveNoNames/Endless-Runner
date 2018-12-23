2186bb01f040bbadf15ad1a0f9f6a9f4b1df592e
TouHouPolice <43275328+TouHouPolice@users.noreply.github.com>
2018-12-23 14:55:58 +0900

    Revert "Revert "12/23/2018""
    This reverts commit c01747cb503ab6dfc29c8c6531fa2bb88153aca5.

---
+++ Endless Runner/Assets/Manager/PlayerStatus.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PlayerStatus {
+
+
+    public static bool vest = false;
+    public static float vestRemaining = 0f;
+    public static bool magicWind = false;
+
+    public static bool magnet = false;
+    public static float magnetRemaining = 0f;
+
+    public static int coinCollected = 0;
+}
---
+++ Endless Runner/Assets/PowerUps/Vest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vest : MonoBehaviour {
+
+    
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            GetVest();
+            Destroy(gameObject);
+        }
+    }
+
+    public void GetVest() //These funciton is called when the powers being picked up
+    {
+        PlayerStatus.vest = true;
+        PlayerStatus.vestRemaining = 30f;
+    }
+}
