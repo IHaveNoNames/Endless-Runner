ce5bb9daf6ef5b8ab0660e96844beefb15440894
TouHouPolice <43275328+TouHouPolice@users.noreply.github.com>
2018-12-23 14:54:29 +0900

    12/23/2018

---
+++ Endless Runner/Assets/Obstacles/Obstacles.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Obstacles : MonoBehaviour {
+
+    
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            GameManager GM = GameObject.Find("GameManager").GetComponent<GameManager>();
+            
+            if (gameObject.CompareTag("HeavyObstacle"))
+            {
+                GM.TakingFatalHit();
+                print("heavy");
+            }
+            else if (gameObject.CompareTag("LightObstacle"))
+            {
+                print("Light");
+                GM.TakingLightHit();
+            }
+
+            Destroy(this.gameObject);
+        }           
+        
+    }
+
+    public void SelfDestoy()
+    {
+        Destroy(this.gameObject);
+    }
+
+    
+}
