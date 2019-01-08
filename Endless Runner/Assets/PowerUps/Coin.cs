9472995f989b1056516bfb5723a1086e8e1beb75
Marblexake <qazx29@gmail.com>
2019-01-08 21:26:25 +0800

    Assets
    there was a new script from the pack for coins, commented the code out, but yall might wanna use it?

--- Endless Runner/Assets/PowerUps/Coin.cs
+++ Endless Runner/Assets/PowerUps/Coin.cs
@@ -1,37 +1,40 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class Coin : MonoBehaviour {
-    private GameObject player;
-    public float attractDistance;
-    public float attractSpeed;
-	// Use this for initialization
-	void Start () {
-        player = GameObject.Find("Player");
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        MagnetAttraction();
-	}
+//public class Coin : MonoBehaviour
+//{
+//    private GameObject player;
+//    public float attractDistance;
+//    public float attractSpeed;
+//    // Use this for initialization
+//    void Start()
+//    {
+//        player = GameObject.Find("Player");
+//    }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            GameStatus.coinCollected += 1;
-            Destroy(gameObject);
-        }
-    }
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        MagnetAttraction();
+//    }
 
-    void MagnetAttraction()
-    {
-        float distance=Vector3.Distance(gameObject.transform.position, player.transform.position);
+//    private void OnTriggerEnter(Collider other)
+//    {
+//        if (other.gameObject.CompareTag("Player"))
+//        {
+//            GameStatus.coinCollected += 1;
+//            Destroy(gameObject);
+//        }
+//    }
 
-        if (GameStatus.magnet==true&&distance < attractDistance)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, attractSpeed * Time.deltaTime);
-        }
-    }
-}
+//    void MagnetAttraction()
+//    {
+//        float distance = Vector3.Distance(gameObject.transform.position, player.transform.position);
+
+//        if (GameStatus.magnet == true && distance < attractDistance)
+//        {
+//            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, attractSpeed * Time.deltaTime);
+//        }
+//    }
+//}
