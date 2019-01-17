6dfc81be2d02582b9bb06aef77645fc139e00a5d
IHaveNoNames <leungweijun@gmail.com>
2019-01-17 14:02:22 +0800

    Jumping and Sliding Animations refinment

--- Endless Runner/Assets/JumpBehaviour.cs
+++ Endless Runner/Assets/JumpBehaviour.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class JumpBehaviour : StateMachineBehaviour {
 
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-    //
+    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+        
     //}
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
@@ -15,10 +16,10 @@ public class JumpBehaviour : StateMachineBehaviour {
     //}
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-
-    //}
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Player.canJump = true;
+    }
 
     // OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
     //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
